4e1aefa8a915ba1eb065d6a2eea668c162633c6b
moyomogi <moyomogi@users.noreply.github.com>
2021-12-16 04:40:31 +0000

    deploy: 491bafc0d4988fcfbee3f059049af7804079156a

---
+++ _nuxt/static/1639629629/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fmillennium-game-dev-dept\u002F_nuxt\u002Fstatic\u002F1639629629",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Fmillennium-game-dev-dept\u002F",assetsPath:"\u002Fmillennium-game-dev-dept\u002F_nuxt\u002F",cdnURL:a}}}}(null));
